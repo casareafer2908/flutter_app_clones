4828316bcdf99c08c4473286e9296d49b05e9afd
Carlos Arellano Casale <40230544+casareafer2908@users.noreply.github.com>
2021-10-04 12:49:48 +0800

    extract custom card

--- lib/src/ui/app_clones/food_panda_screen.dart
+++ lib/src/ui/app_clones/food_panda_screen.dart
@@ -63,68 +63,81 @@ class _FoodPandaScreenState extends State<FoodPandaScreen> {
   }
 
   Widget _buildContents(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Card(
-          color: Color.fromRGBO(215, 15, 100, .9),
-          child: InkWell(
-            onTap: () {},
-            child: Padding(
-              padding: EdgeInsets.all(14),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: SizedBox(
-                      height: 120,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const <Widget>[
-                          Text(
-                            "Food delivery",
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: .5,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(height:10),
-                          Text(
-                            'Order food you love',
-                            style: TextStyle(
-                              letterSpacing: .5,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Container(
-                        height: 120,
-                        child: Image.asset('assets/images/characters/boo.png')),
-                  ),
-                ],
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(14),
+      child: Column(
+        children: <Widget>[
+          customCard(),
+          const SizedBox(
+            height: 14,
+          ),
+          customCard(),
+          const Expanded(child: SizedBox(height: 10)),
+          Expanded(
+            child: StaggeredGridView.count(
+              crossAxisCount: 4,
+              staggeredTiles: _staggeredTiles,
+              mainAxisSpacing: 4,
+              crossAxisSpacing: 4,
+              padding: const EdgeInsets.all(4),
+              children: _tiles,
             ),
           ),
-        ),
-        Expanded(child: Container(color: Colors.red)),
-        const Expanded(child: SizedBox(height: 10)),
-        Expanded(
-          child: StaggeredGridView.count(
-            crossAxisCount: 4,
-            staggeredTiles: _staggeredTiles,
-            mainAxisSpacing: 4,
-            crossAxisSpacing: 4,
-            padding: const EdgeInsets.all(4),
-            children: _tiles,
+        ],
+      ),
+    );
+  }
+
+  Card customCard() {
+    return Card(
+      color: const Color.fromRGBO(215, 15, 100, .9),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: EdgeInsets.all(14),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: SizedBox(
+                  height: 120,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Text(
+                        "Food delivery",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: .5,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Order food you love',
+                        style: TextStyle(
+                          letterSpacing: .5,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                    height: 120,
+                    child: Image.asset('assets/images/characters/boo.png')),
+              ),
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
